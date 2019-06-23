462eb38d64fb97bf3bbb2376e9a22aa940c8bff8
Gaurav Singh <gks.com131@gmail.com>
2019-06-23 15:17:37 +0530

    string output in numer pattern

--- number pattern.py
+++ number pattern.py
@@ -1,12 +1,14 @@
 n = 5 #user input
 b = (n*2)-1 
-a = [n]*b
+a = [str(n)]*b
 mn = [a]
 for i in range(1,(b//2)+1):
 	c = a[i:-i]
 	for x in range(len(c)):
-		c[x] = n - i
+		c[x] = str(n - i)
 	mn.append(mn[-1][:i]+c+mn[-1][-i:])
 mn2 = mn[:-1]
 mn2.reverse()
-mn.append(mn2)
+mn.extend(mn2)
+for x in mn:
+	print("".join(x))
