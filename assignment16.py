8fd2d2416db4c1d9328cad9d32e4fa468a33f792
Gaurav Singh <gks.com131@gmail.com>
2019-06-24 21:32:56 +0530

    rupees problem updated

--- assignment16.py
+++ assignment16.py
@@ -1,17 +1,28 @@
-def payment(coins,notes,amount):
-    amount_1 = coins
-    amount_5 = notes * 5
-    if (amount_5+amount_1)< amount:
+def make_amount(rupees_to_make,no_of_five,no_of_one):
+    five_needed=0
+    one_needed=0
+    amount_1 = no_of_one
+    amount_5 = no_of_five * 5
+    if (amount_5+amount_1)< rupees_to_make:
+        print(-1)
+        return
+    five_needed = rupees_to_make//5
+    one_needed = rupees_to_make%5
+    if amount_5<rupees_to_make:
+        one_needed = rupees_to_make-amount_5
+        print("No. of Five needed :", no_of_five)
+        print("No. of One needed  :", one_needed)
+        return
+    if amount_1<one_needed:
+        print(-1)
         return -1
-    pd5 = amount//5
-    remain = amount%5
-    if amount_5<amount:
-        remain_m = amount-amount_5
-        return ("5rs note:-",notes,"1rs coin:-",remain_m)
-    if amount_1<remain:
-        return -1
-    return ("5rs note:-",pd5,"1rs coin:-",remain)
-print(payment(5,20,105))
-    
-        
+
+    print("No. of Five needed :", five_needed)
+    print("No. of One needed  :", one_needed)
+    #print(-1)
+
+
+#Provide different values for rupees_to_make,no_of_five,no_of_one and test your program
+make_amount(28,8,5)    
+
     
